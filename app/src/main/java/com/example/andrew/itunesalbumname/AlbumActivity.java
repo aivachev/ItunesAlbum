074df90f87668cb70d57df85f5e515066b5fc523
Andrew Ivachev <g00df3llow@mail.ru>
2018-07-10 14:13:45 +0300

    implements CoordinatorLayout

--- app/src/main/java/com/example/andrew/itunesalbumname/AlbumActivity.java
+++ app/src/main/java/com/example/andrew/itunesalbumname/AlbumActivity.java
@@ -1,53 +1,75 @@
 package com.example.andrew.itunesalbumname;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.example.andrew.itunesalbumname.model.AlbumDetail;
 import com.example.andrew.itunesalbumname.model.AlbumDetailResponse;
 import com.example.andrew.itunesalbumname.ui.presenters.AlbumPresenter;
 import com.example.andrew.itunesalbumname.ui.interfaces.AlbumViewInterface;
 import com.example.andrew.itunesalbumname.ui.adapters.AlbumAdapter;
-import com.example.andrew.itunesalbumname.utils.MyDateUtils;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import jp.wasabeef.picasso.transformations.BlurTransformation;
 
-public class AlbumActivity extends AppCompatActivity implements AlbumViewInterface {
+public class AlbumActivity extends AppCompatActivity implements AlbumViewInterface, AppBarLayout.OnOffsetChangedListener {
     @BindView(R.id.song_recycler_view)
     RecyclerView songsRecyclerView;
     @BindView(R.id.album_name_text_view)
     TextView album_name;
     @BindView(R.id.album_image_view)
     ImageView albumImageView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
     @BindView(R.id.artist_name_text_view)
     TextView artist_name;
-    @BindView(R.id.copyright_text_view)
-    TextView copyright;
-    @BindView(R.id.release_date_text_view)
-    TextView release_date;
+    //@BindView(R.id.copyright_text_view)
+    //TextView copyright;
+    //@BindView(R.id.release_date_text_view)
+    //TextView release_date;
+    @BindView(R.id.appbar)
+    AppBarLayout appBarLayout;
+    @BindView(R.id.album_top_image)
+    ImageView albumTopImage;
 
     private AlbumAdapter albumAdapter;
     private AlbumPresenter albumPresenter;
+    private int mMaxScrollSize;
+    private static final int PERCENTAGE_TO_ANIMATE_ALBUM = 20;
+    private boolean mIsAlbumShown = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
 
         setupMVP();
         setupViews();
+
+        appBarLayout.addOnOffsetChangedListener(this);
+        mMaxScrollSize = appBarLayout.getTotalScrollRange();
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
@@ -113,13 +135,37 @@ public class AlbumActivity extends AppCompatActivity implements AlbumViewInterfa
     private void displayAlbumInfo(AlbumDetail alb) {
         album_name.setText(alb.getCollectionCensoredName());
         artist_name.setText(alb.getArtistName());
-        release_date.setText(MyDateUtils.getFormattedDate(alb.getReleaseDate()));
-        copyright.setText(alb.getCopyright());
-        Picasso.get().load(alb.getArtworkUrl100()).into(albumImageView);
+        Picasso.get()
+                .load(alb.getArtworkUrl100())
+                .transform(new BlurTransformation(getApplicationContext(), 5, 2))
+                .into(albumImageView);
+        Picasso.get().load(alb.getArtworkUrl100()).into(albumTopImage);
     }
 
     @Override
     public void displayError(String e) {
         showToast(e);
     }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        if (mMaxScrollSize == 0)
+            mMaxScrollSize = appBarLayout.getTotalScrollRange();
+        int percentage = (Math.abs(verticalOffset)) * 100 / mMaxScrollSize;
+        if (percentage >= PERCENTAGE_TO_ANIMATE_ALBUM && mIsAlbumShown) {
+            mIsAlbumShown = false;
+            albumTopImage.animate()
+                    .scaleY(0).scaleX(0)
+                    .setDuration(200)
+                    .start();
+        }
+        if (percentage <= PERCENTAGE_TO_ANIMATE_ALBUM && !mIsAlbumShown) {
+            mIsAlbumShown = true;
+            albumTopImage.animate()
+                    .scaleY(1).scaleX(1)
+                    .start();
+        }
+    }
+
+
 }
